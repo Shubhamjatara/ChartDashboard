1a98b682a7c52aa263784ff11af0f108aff9bd57
shubham <bhadoriya.shubham1995@gmail.com>
2023-07-18 18:26:23 +0530

    dashboard with various charts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Container, Row, Col } from 'react-bootstrap';
+//import Button from 'react-bootstrap/Button';
+import Pie from './chartsComponents/Piechart';
+import DoughnutChart from './chartsComponents/doughnutChart';
+import BarChart from './chartsComponents/BarChart';
+import LineChart from './chartsComponents/lineChart';
+import AreaChart from './chartsComponents/area';
+import BubbleChart from './chartsComponents/BubbleChart';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container className=' mt-2 p-0'>
+        <Row className='justify-content-center align-items-center'>
+          <Col className='justify-content-center align-items-center d-flex'>
+            <Pie />
+          </Col>
+          <Col className='justify-content-center align-items-center d-flex'>
+            <DoughnutChart />
+          </Col>
+
+          <Col className='justify-content-center align-items-center d-flex'>
+          <DoughnutChart />
+          </Col>
+        
+        </Row>
+
+        <Row className='mt-4 '>
+          <Col className='justify-content-center align-items-center d-flex'>
+          <BarChart/>
+          </Col>
+
+          <Col className='justify-content-center align-items-center d-flex' >
+          <LineChart />
+          </Col>
+        </Row>
+
+        <Row className='mt-3'>
+          <Col className='justify-content-center align-items-center d-flex'>
+            <AreaChart/>
+          </Col>
+
+          <Col className='justify-content-center align-items-center d-flex' >
+          <BubbleChart />
+          </Col>
+        </Row>
+
+
+
+      </Container>
     </div>
+
   );
 }
 
